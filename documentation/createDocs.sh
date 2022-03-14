fe6c64a5ddea825847f1607f812f9269722f9aae
newHeiko <35038740+newHeiko@users.noreply.github.com>
2022-03-14 20:39:59 +0100

    Remove old documentation taken over from wiFred

--- documentation/createDocs.sh
+++ documentation/createDocs.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
-echo "Reducing eps image sizes"
-
-for i in images/*eps
+echo "Creating png files from pdf files"
+cd largeImages
+for i in *pdf
 do
-	ps2ps $i $i.temp
-	mv $i.temp $i
+    PNGFILE=../images/`basename $i .pdf`.png
+    if [ $i -nt $PNGFILE ]
+    then
+	convert -density 150 $i $PNGFILE
+    fi
 done
-
-echo "Creating all docs from latex source file"
-pdflatex docu.tex
-pdflatex docu.tex
-gs -sDEVICE=pdfwrite -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=docu.pdf.temp docu.pdf
-mv docu.pdf.temp docu.pdf
-latex docu.tex
-latex docu.tex
-cd html
-imagen ../docu.tex
-hevea article.hva ../docu.tex
 cd ..
+
+echo "Reducing jpg images in size"
+cd largeImages
+for i in *jpg
+do
+    if [ $i -nt ../images/$i ]
+    then
+	convert $i -resize 1920x1920 -quality 50 -strip ../images/$i
+    fi
+done
+cd ..
+
+echo "Creating html and pdf files"
+asciidoctor -b docbook docu_en.adoc
+asciidoctor -b docbook docu_de.adoc
+asciidoctor -b html5 docu_en.adoc
+asciidoctor -b html5 docu_de.adoc
+dblatex -P figure.note="/etc/asciidoc/images/icons/note" -P figure.important="/etc/asciidoc/images/icons/important" -P figure.warning="/etc/asciidoc/images/icons/warning" -P doc.collab.show=0 -P paper.type=a4paper -P page.margin.top=15mm -P page.margin.bottom=15mm -P page.margin.inner=20mm -P page.margin.outer=20mm docu_en.xml 
+dblatex -P figure.note="/etc/asciidoc/images/icons/note" -P figure.important="/etc/asciidoc/images/icons/important" -P figure.warning="/etc/asciidoc/images/icons/warning" -P doc.collab.show=0 -P paper.type=a4paper -P page.margin.top=15mm -P page.margin.bottom=15mm -P page.margin.inner=20mm -P page.margin.outer=20mm docu_de.xml
+
+
+exit
